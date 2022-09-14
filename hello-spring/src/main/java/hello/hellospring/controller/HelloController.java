180a87028b9645fe144a1f9dc740df3365090564
SsoHhyun <janeqcitizen@naver.com>
2022-09-14 23:46:20 +0900

    Spring Basic

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,17 +3,48 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "hello!!");
         return "hello";
     }
     // 스프링부트 thymeleaf 템플릿 엔진
     // templates 이하의 hello라는 이름의 파일을 찾아 반환해 줌 -> 화면에 hello.html이 나타남
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString (@RequestParam("name") String name) {
+        // 데이터를 그대로 내려줌, html이 안 뜸
+        return "hello " + name; //"hello spring"
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
 
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
